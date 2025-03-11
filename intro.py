8fcddbaa71857edd241556cc88915a629454b067
anyangumajimbo <majimboanyangu50@gmail.com>
2025-03-11 11:20:35 +0300

    Made some changes

--- intro.py
+++ intro.py
@@ -1,11 +1,7 @@
-# Variables hold data
-age = 25          # int (integer)
-height = 5.75      # float (decimal)
-name = "Charlie"   # str (string)
-is_student = True  # bool (boolean)
+print("Hello world")
 
-# Printing out the values
-print("Name:", name)          # Outputs: Name: Charlie
-print("Age:", age)            # Outputs: Age: 25
-print("Height:", height)      # Outputs: Height: 5.75
-print("Is a student?", is_student)  # Outputs: Is a student? True
+number = 10
+print(number)
+
+float = 0.03
+print(float)
